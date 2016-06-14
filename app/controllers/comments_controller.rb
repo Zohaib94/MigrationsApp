6c10bdfdb5350d489aee73f5d1253189228ecc9d
Zohaib <zohaibahmedbutt@gmail.com>
2016-06-14 11:30:08 +0500

    Show method and comments/show.html.erb fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,7 @@ class CommentsController < ApplicationController
   # GET /comments/1
   # GET /comments/1.json
   def show
+    @article = Article.find(params[:article_id])
     @comment = Comment.find(params[:id])
 
     respond_to do |format|
