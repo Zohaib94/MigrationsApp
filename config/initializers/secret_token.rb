59e6e23768efdbe704c1d97a8371a781b3cc1e81
Zohaib <zohaibahmedbutt@gmail.com>
2016-06-14 10:16:54 +0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MigrationsApp::Application.config.secret_token = 'c543b94cae7a203abb0ee744fbe6a363a714e8bcb24685326d9dc1db6d37755b90b15a7a262c6d67b6fa374962b649e5027ad564d2f4c82880090f67535e9913'
