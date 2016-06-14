8eef50760636cbed619090ceed83b311f78d16e3
Zohaib <zohaibahmedbutt@gmail.com>
2016-06-14 11:00:17 +0500

    Added Comments Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160614055846) do
+ActiveRecord::Schema.define(:version => 20160614055938) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(:version => 20160614055846) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "comments", :force => true do |t|
+    t.text     "comment"
+    t.integer  "article_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "comments", ["article_id"], :name => "index_comments_on_article_id"
 
   create_table "items", :force => true do |t|
     t.string "name", :null => false
